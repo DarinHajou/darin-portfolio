692e36c9bb1c9f2c578bea2aae65fbba21e16b41
Darin Hajou <darinhajou@gmail.com>
2026-08-16 21:55:39 +0200

    Refactor app to only import AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
 import './App.css'
-import Navigation from './sections/Navigation/Navigation';
-import Hero from './sections/Hero/Hero';
-import MapOfPi from './projects/MapOfPi/MapOfPi';
-import ResetWithContext from "./projects/ResetWithContext/ResetWithContext";
-import Whisper from './projects/Whisper/Whisper';
-
+import { AppRoutes } from "./routes";
 function App() {
-  return (
-    <>
-      <Navigation />
-
-      <main id="top">
-        <Hero />
-
-        <MapOfPi />
-
-        <ResetWithContext/>
-
-        <Whisper />
-
-        <section id="about" className="page-container page-section">
-          <h2 className="heading-section">About</h2>
-        </section>
-
-        <section id="contact" className="page-container page-section">
-          <h2 className="heading-section">Contact</h2>
-        </section>
-      </main>
-    </>
-  );
+  return <AppRoutes />
 }
 
 export default App;
