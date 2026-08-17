f9bef6c491c4681c35d491be4509a763767bc0d0
Darin Hajou <darinhajou@gmail.com>
2026-08-18 00:36:33 +0200

    Add contact component to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import Navigation from "../sections/Navigation/Navigation"
 import Hero from "../sections/Hero/Hero"
 import About from "../sections/About/About";
+import Contact from "../sections/Contact/Contact";
 
 function Home() {
   return (
     <>
       <Navigation />
       <Hero />
-      {/* // <Contact /> */}
      <About />
+     <Contact />
     </>
   );
 }
