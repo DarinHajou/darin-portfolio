ea5742c67ff026d420677ffca243ac0f7d60ab2d
Darin Hajou <darinhajou@gmail.com>
2026-08-20 01:13:29 +0200

    Add contact section skeleton

---
+++ src/sections/Contact/Contact.tsx
@@ -0,0 +1,15 @@
+import styles from "./Contact.module.css"
+
+function Contact() {
+  return (
+  <section 
+    id="contact" 
+    className={styles.contactTitle} 
+    arial-labelledby
+  >
+    <h1>Contact</h1>
+  </section>
+  );
+}
+
+export default Contact;
